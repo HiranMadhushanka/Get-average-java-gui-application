2d691c9b7971eb14989b09121e3a91bc5ff70107
Hiran Madhushanka <hiranmadhushanka123@gmail.com>
2018-11-19 21:26:14 -0800

    get average 1.0

---
+++ src/getaverage/GetAverage.java
@@ -0,0 +1,12 @@
+
+package getaverage;
+
+
+public class GetAverage {
+
+    
+    public static void main(String[] args) {
+        Average a = new Average ();
+        a.setVisible(true);
+    }
+}
